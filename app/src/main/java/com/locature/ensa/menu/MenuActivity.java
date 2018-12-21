635494f3021ef6a1a61b066c8235b0db02b47b69
El-MehdiAchahboun <elmehdiachahboun@gmail.com>
2018-12-21 20:58:29 +0100

    first commit

---
+++ app/src/main/java/com/locature/ensa/menu/MenuActivity.java
@@ -0,0 +1,79 @@
+package com.locature.ensa.menu;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.locature.ensa.menu.fragments.CartFragment;
+import com.locature.ensa.menu.fragments.ListFragment;
+import com.locature.ensa.menu.fragments.MapFragment;
+import com.locature.ensa.menu.fragments.ProfileFragment;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_map:
+                    toolbar.setTitle("Map");
+                    fragment = new MapFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_list:
+                    toolbar.setTitle("List");
+                    fragment = new ListFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_cart:
+                    toolbar.setTitle("Cart");
+                    fragment = new CartFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_profile:
+                    toolbar.setTitle("Profile");
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        toolbar = getSupportActionBar();
+        // load the Map fragment by default
+        toolbar.setTitle("Map");
+        loadFragment(new MapFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+    }
+
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_container, fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+}
